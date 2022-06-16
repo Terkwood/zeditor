fe72288c65405d9e66ea9682bc5597853ff10f50
terkwood <38859656+Terkwood@users.noreply.github.com>
2022-06-16 17:26:25 -0400

    columns

--- src/config_screen.rs
+++ src/config_screen.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 
 use crate::{db::Db, screens::ZeditorScreens};
 use cursive::{
@@ -7,27 +10,20 @@ use cursive::{
     Cursive,
 };
 
-const INPUTS: &str = "configurable search and replace inputs";
+const EXISTING_SEARCH_INPUTS: &str = "existing search inputs";
+const EXISTING_REPLACE_INPUTS: &str = "existing replace inputs";
+const NEW_SEARCH_INPUT: &str = "new search input";
+const NEW_REPLACE_INPUT: &str = "new replace input";
 
 pub fn render(siv: &mut Cursive, screens: ZeditorScreens, db: Arc<Mutex<Db>>) {
     siv.set_screen(screens.config);
 
     use cursive::utils::markup::StyledString;
 
-    let mut existing_search_inputs = ListView::new();
-    let mut replace_inputs = ListView::new();
-
-    if let Ok(sr) = db.lock().unwrap().get_search_replace() {
-        for (search, replace) in sr {
-            existing_search_inputs.add_child("", TextView::new(search));
-
-            replace_inputs.add_child("", {
-                let mut rta = TextArea::new();
-                rta.set_content(replace);
-                rta
-            });
-        }
-    }
+    let existing_search_inputs = ListView::new().with_name(EXISTING_SEARCH_INPUTS);
+    let existing_replace_inputs = ListView::new().with_name(EXISTING_REPLACE_INPUTS);
+    let new_search_input = TextArea::new().with_name(NEW_SEARCH_INPUT);
+    let new_replace_input = TextArea::new().with_name(NEW_REPLACE_INPUT);
 
     let inputs_with_header = ScrollView::new(
         LinearLayout::horizontal()
@@ -37,6 +33,7 @@ pub fn render(siv: &mut Cursive, screens: ZeditorScreens, db: Arc<Mutex<Db>>) {
                         "SEARCH",
                         cursive::theme::Effect::Bold,
                     )))
+                    .child(new_search_input)
                     .child(existing_search_inputs),
             )
             .child(
@@ -45,7 +42,8 @@ pub fn render(siv: &mut Cursive, screens: ZeditorScreens, db: Arc<Mutex<Db>>) {
                         "REPLACE",
                         cursive::theme::Effect::Bold,
                     )))
-                    .child(replace_inputs),
+                    .child(new_replace_input)
+                    .child(existing_replace_inputs),
             ),
     );
 
@@ -60,4 +58,32 @@ pub fn render(siv: &mut Cursive, screens: ZeditorScreens, db: Arc<Mutex<Db>>) {
         )
         .title("zeditor"),
     );
+
+    if let Ok(sr) = db.lock().unwrap().get_search_replace() {
+        update_search_inputs(siv, &sr);
+        update_replace_inputs(siv, &sr);
+        
+    }
+}
+pub fn update_search_inputs(siv: &mut Cursive, sr: &HashMap<String, String>) {
+    let mut inputs = siv.find_name::<ListView>(EXISTING_SEARCH_INPUTS).unwrap();
+    inputs.clear();
+
+
+    for (key, _) in sr {
+        inputs.add_child("",TextView::new(key));
+    }   
+}
+
+pub fn update_replace_inputs(siv: &mut Cursive, search_replace: &HashMap<String, String>) {
+    let mut replace_inputs = siv.find_name::<ListView>(EXISTING_REPLACE_INPUTS).unwrap();
+    replace_inputs.clear();
+
+    for (search, replace) in search_replace {
+        replace_inputs.add_child("", {
+            let mut rta = TextArea::new();
+            rta.set_content(replace);
+            rta
+        });
+    }
 }
